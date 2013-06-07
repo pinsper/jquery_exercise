a3a5449dd7f3d62dab1a6c7feb89363faa81632f
laras126 <laras126@gmail.com>
2013-06-06 21:32:38 -0400

    FARTSCROLL

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
-$(document).ready(function(){
+fartscroll();
 
+$(document).ready(function(){
 
 	$('#changePictureBtn').click(function(){
 		$('.avatar').attr('src', 'img/harry.jpg');
 		$(this).removeClass('button').text("Wait, that's not Sam!");
+		$('.quotes').css('color', 'green').append('<img src="img/harry.jpg" />');
 	});
+
+	$('.avatar').hide().slideDown(500).css('border','5px green dashed');
+
+	// Plugins
+
+	// Farting sounds
+	 
 
 });
